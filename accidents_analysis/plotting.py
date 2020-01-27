b0f3ae1ee6854356d3139b80715b26cf8474a72a
razmikmelikbekyan <melikbekyan@yahoo.com>
2020-01-27 10:03:57 +0100

    FEAT: analysis

--- accidents_analysis/plotting.py
+++ accidents_analysis/plotting.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Dict
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,7 +6,7 @@ import seaborn as sns
 
 
 def plot_distplot(df: pd.DataFrame,
-                  plotting_column: str,
+                  x_column: str,
                   hue_column: str = None,
                   norm_hist: bool = False,
                   hist: bool = False,
@@ -19,7 +19,7 @@ def plot_distplot(df: pd.DataFrame,
     if hue_column:
         for x in df[hue_column].unique():
             sns.distplot(
-                df[df[hue_column] == x][plotting_column],
+                df[df[hue_column] == x][x_column],
                 hist=hist,
                 kde=kde,
                 kde_kws={'linewidth': 3, 'shade': True},
@@ -28,7 +28,7 @@ def plot_distplot(df: pd.DataFrame,
             )
     else:
         sns.distplot(
-            df[plotting_column],
+            df[x_column],
             hist=hist,
             kde=kde,
             kde_kws={'linewidth': 3, 'shade': True},
@@ -39,8 +39,74 @@ def plot_distplot(df: pd.DataFrame,
     if legend:
         plt.legend(prop={'size': 16})
     plt.title(f'Density Plot vs {hue_column}' if hue_column else 'Density Plot')
-    plt.xlabel(plotting_column)
+    plt.xlabel(x_column)
     plt.ylabel('Density')
     if output_path:
         plt.savefig(output_path, bbox_inches='tight')
+    plt.show()
+
+
+def plot_normalised_barplot(df: pd.DataFrame,
+                            x_column: str,
+                            hue_column: str,
+                            title: str = None,
+                            figsize: Tuple[int, int] = (14, 8),
+                            palette: Dict = None,
+                            legend: bool = True,
+                            output_path: str = None):
+    """
+    Plots percentage of every category from hue_column in division of categories from x_column.
+    """
+    title = title if title else f'percentage of {hue_column} in {x_column}'
+    x, y, hue = x_column, "percentage", hue_column
+    normalized_df = (df[hue]
+                     .groupby(df[x])
+                     .value_counts(normalize=True)
+                     .rename(y)
+                     .reset_index())
+
+    plt.figure(figsize=figsize)
+    plt.title(title, fontweight='bold', fontsize=16)
+    ax = sns.barplot(x=x, y=y, hue=hue, data=normalized_df, palette=palette)
+    for p in ax.patches:
+        ax.annotate(
+            format(p.get_height(), '.2f'),
+            (p.get_x() + p.get_width() / 2., p.get_height()),
+            ha='center',
+            va='center',
+            xytext=(0, 10),
+            textcoords='offset points'
+        )
+    if legend:
+        plt.legend(loc="upper right", bbox_to_anchor=(1, 1))
+    else:
+        ax.get_legend().set_visible(False)
+    if output_path:
+        plt.savefig(output_path, bbox_inches='tight')
+    plt.show()
+
+
+def plot_countplot(df: pd.DataFrame,
+                   x_column: str,
+                   hue_column: str = None,
+                   title: str = None,
+                   figsize: Tuple[int, int] = (14, 8),
+                   palette: Dict = None,
+                   output_path: str = None):
+    """
+    Plots count plot.
+    """
+    title = title if title else f'Count of {x_column}'
+
+    plt.figure(figsize=figsize)
+    plt.title(title, fontweight='bold', fontsize=16)
+    sns.countplot(x=x_column, y=None, hue=hue_column, data=df, palette=palette)
+    plt.xticks(rotation=90, fontsize=10, fontweight='bold')
+    plt.xlabel(x_column, fontsize=14)
+    plt.ylabel('count', fontsize=14)
+
+    if output_path:
+        plt.savefig(output_path, bbox_inches='tight')
+
+    plt.tight_layout()
     plt.show()
--- accidents_analysis/utils.py
+++ accidents_analysis/utils.py
@@ -1,5 +1,7 @@
 import json
+from typing import Iterable, Tuple
 
+import editdistance
 import mysql.connector
 import pandas as pd
 
@@ -10,3 +12,32 @@ def sql_table_to_pandas(db_config_path: str, table_name: str) -> pd.DataFrame:
 
     cnx = mysql.connector.connect(**db_config)
     return pd.read_sql(f"SELECT * FROM {table_name}", cnx)
+
+
+def str2str_match_ratio(s_1: str, s_2: str) -> float:
+    """Calculates 2 string match ratio using given method."""
+
+    def _process(s: str) -> str:
+        if not s:
+            return ''
+        return ' '.join(s.split()).replace('.', '')
+
+    s_1, s_2 = _process(s_1), _process(s_2)
+
+    if not s_1 or not s_2:
+        return 0.
+
+    return 1 - editdistance.eval(s_1, s_2) / max(len(s_1), len(s_2))
+
+
+def str2iter_match_ratio(s: str, iterable_s: Iterable) -> Tuple[str, float]:
+    """
+    Find the item from the given iterable of strings which has highest match ratio comparing with
+    the base string and return the best match and it's match ratio.
+    """
+    if all(False for _ in iterable_s):
+        return '', 0.
+
+    matches = [(x, str2str_match_ratio(s, x)) for x in iterable_s]
+    matches.sort(key=lambda x: x[1])
+    return matches[-1]
